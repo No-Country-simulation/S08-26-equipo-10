de7588f37a355efea31a429ce12d7be13e5cfcd7
pauaranw <paulinaarandawastavino@gmail.com>
2026-09-10 21:25:06 -0300

    fix: agregar anotacion Service al servicio de tipo de servicio

--- backend/src/main/java/com/fieldflow/workorders/application/ServiceTypeServiceImpl.java
+++ backend/src/main/java/com/fieldflow/workorders/application/ServiceTypeServiceImpl.java
@@ -2,9 +2,12 @@ package com.fieldflow.workorders.application;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.fieldflow.workorders.persistence.ServiceTypeRepository;
 import com.fieldflow.workorders.domain.ServiceType;
 
+@Service 
 public class ServiceTypeServiceImpl implements ServiceTypeService{
 
     private final ServiceTypeRepository repository;
